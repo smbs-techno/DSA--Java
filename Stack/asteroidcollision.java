4776d0ab2e7f094ca0b37342f912c873b2813515
“smbs-techno” <srimashissmbs2005@gmail.com>
2026-05-22 12:35:18 +0530

    .

--- Stack/asteroidcollision.java
+++ Stack/asteroidcollision.java
@@ -14,7 +14,6 @@ public class asteroidcollision {
                 else if(st.isEmpty() || st.peek()<0) st.push(asteroids[i]);
             }
         }
-        
         int[] ans=new int[st.size()];
         for(int i=st.size()-1;i>=0;i--){
             ans[i]=st.pop();
