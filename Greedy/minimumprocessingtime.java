ce4cd4605ca29777dc589869fb557f8125008cb7
smbs-techno <srimashissmbs2005@gmail.com>
2026-07-14 19:55:17 +0530

    GREEDY

--- Greedy/minimumprocessingtime.java
+++ Greedy/minimumprocessingtime.java
@@ -1,5 +1,6 @@
 package Greedy;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,5 +16,18 @@ public class minimumprocessingtime {
         }
         return max;
     }
-    
+    public static void main(String[] args) {
+        ArrayList<Integer> pt=new ArrayList<>();
+        ArrayList<Integer> t=new ArrayList<>();
+        pt.add(8);
+        pt.add(10);
+        t.add(2);
+        t.add(2);
+        t.add(3);
+        t.add(1);
+        t.add(8);
+        t.add(7);
+        t.add(4);
+        t.add(5);
+    }
 }
