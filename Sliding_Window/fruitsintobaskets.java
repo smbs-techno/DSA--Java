8a3c3625560cf7ff48caf4d3b99bcdba2cd9387a
smbs-techno <srimashissmbs2005@gmail.com>
2026-08-16 00:21:45 +0530

    Sliding Window & HashMap

---
+++ Sliding_Window/fruitsintobaskets.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+public class fruitsintobaskets {
+    public static int totalFruit(int[] fruits) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int left=0;
+        int max=0;
+        for(int right=0;right<fruits.length;right++){
+            map.put(fruits[right],map.getOrDefault(fruits[right],0)+1);
+            while(map.size()>2){
+                map.put(fruits[left],map.get(fruits[left])-1);
+                if(map.get(fruits[left])==0) map.remove(fruits[left]);
+                left++;
+            }
+            max=Math.max(max,right-left+1);
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] fruits={1,2,3,2,2};
+        System.out.println(totalFruit(fruits));
+    }
+}
