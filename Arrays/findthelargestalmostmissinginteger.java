ad57b2bce8c78a7bc0ac631cddba22966d440027
smbs-techno <srimashissmbs2005@gmail.com>
2026-08-18 12:42:29 +0530

    Array

---
+++ Arrays/findthelargestalmostmissinginteger.java
@@ -0,0 +1,39 @@
+
+import java.util.HashMap;
+public class findthelargestalmostmissinginteger {
+    public static int largestInteger(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        if (k == nums.length) {
+            int max = -1;
+            for (int num : nums) {
+                max = Math.max(max, num);
+            }
+            return max;
+        }
+        if (k == 1) {
+            int max = -1;
+            for (int num : nums) {
+                if (map.get(num) == 1) {
+                    max = Math.max(max, num);
+                }
+            }
+            return max;
+        }
+        int ans = -1;
+        if (map.get(nums[0]) == 1) {
+            ans = Math.max(ans, nums[0]);
+        }
+        if (map.get(nums[nums.length - 1]) == 1) {
+            ans = Math.max(ans, nums[nums.length - 1]);
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums={3,9,7,2,1,7};
+        int k = 4;
+        System.out.println(largestInteger(nums, k));
+    }
+}
